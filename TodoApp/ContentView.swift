7de23011813090cbf020baec6a5d2b947c0b019f
NaoKanazawa <naomiti3333@gmail.com>
2020-04-11 17:24:31 +0900

    Initial Commit

---
+++ TodoApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoApp
+//
+//  Created by 金澤直 on 2020/04/11.
+//  Copyright © 2020 Nao. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
